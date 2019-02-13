5ec39312cf60bb0db5334287576706ab935c7e2a
Niemuuu <yorori.too@gmail.com>
2019-02-13 21:52:21 +0900

    Divide the main process as a function

---
+++ cmd/proc.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/niemuuu/yapla/pkg/mpv"
+	"github.com/niemuuu/yapla/pkg/youtube"
+	"github.com/spf13/cobra"
+)
+
+// RunProcess does the main process
+func RunProcess(cmd *cobra.Command, args []string) {
+	query, err := cmd.PersistentFlags().GetString("query")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	if strings.Trim(query, " ") == "" {
+		fmt.Fprint(os.Stderr, `Please specify the search query with '-q' option, like: [yapla -q "Hip Hop"]`)
+		return
+	}
+
+	youtubeSvc, err := youtube.NewService()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	list, err := youtubeSvc.SearchWithQuery(query)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	urls, err := youtube.BuildURLsFromItems(list.Items)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	err = mpv.Play(urls...)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+}
--- cmd/root.go
+++ cmd/root.go
@@ -3,53 +3,14 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"strings"
 
-	"github.com/niemuuu/yapla/pkg/mpv"
-	"github.com/niemuuu/yapla/pkg/youtube"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "yapla",
 	Short: "Simple Youtube Audio Player",
-	Run: func(cmd *cobra.Command, args []string) {
-
-		query, err := cmd.PersistentFlags().GetString("query")
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			return
-		}
-		if strings.Trim(query, " ") == "" {
-			fmt.Fprint(os.Stderr, `Please specify the search query with '-q' option, like: [yapla -q "Hip Hop"]`)
-			return
-		}
-
-		youtubeSvc, err := youtube.NewService()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			return
-		}
-
-		list, err := youtubeSvc.SearchWithQuery(query)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			return
-		}
-
-		urls, err := youtube.BuildURLsFromItems(list.Items)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			return
-		}
-
-		err = mpv.Play(urls...)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			return
-		}
-
-	},
+	Run:   func(cmd *cobra.Command, args []string) { RunProcess(cmd, args) },
 }
 
 // Execute root command
